4cdfaf69cafdbe93659628ae5e273bbcc785d44f
Chumleee <jose.gonzalez6117@lumnos.udg.mx>
2025-07-10 20:43:42 -0600

    operadores logicos

---
+++ DAY_4/Operadores_logicos.py
@@ -0,0 +1,18 @@
+'''crea 3 variables (num1,num2 y num3):
+dentro de num1 almacena 36
+dentro de num 2 almacena 72/2
+dentro de num 3 almacena 48
+
+verifica si num1 es mayor que num2 y menor que num3
+
+almacaena el resultado en mi_bool
+'''
+
+
+num1 = 36
+num2 = 72/2
+num3 = 48
+
+mi_bool = (num1 > num2) and (num1 < num3)
+
+print(mi_bool)
