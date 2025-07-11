e3d9ec92af8ac23aec2bdc86be2d921cd230e86e
Chumleee <88804191+Chumleee@users.noreply.github.com>
2025-07-10 23:50:52 -0600

    nuevos archivos fluo

---
+++ DAY_4/flujo.py
@@ -0,0 +1,21 @@
+'''mascota = 'perro'
+
+if mascota == 'gato':
+    print('tienes un gato')
+elif mascota == 'perro':
+    print('tienes un perro')
+else:
+    print('no se que mascota tienes')'''
+
+edad = 16
+calificacion = 9
+
+if edad < 18:
+    print('eres menor de edad')
+    if calificacion >= 7:
+        print('aprobado')
+    else:
+        print('reprobado')
+else:
+    print('eres mayor de edad')
+
---
+++ DAY_4/flujo_3.py
@@ -0,0 +1,15 @@
+'''Para acceder a un determinado puesto de trabajo,
+el candidato debe ser capaz de programar en Python y tener
+ conocimientos de inglés.
+
+Crea una estructura condicional para evaluar a un candidato dadas
+estas condiciones, y muestra el mensaje correspondiente en pantalla:
+
+    "Cumples con los requisitos para postularte"
+
+    "Para postularte, necesitas saber programar en Python y t
+    ener conocimientos de inglés"
+
+    "Para postularte, necesitas tener conocimientos de inglés"
+
+    "Para postularte, necesitas saber programar en Python"'''
